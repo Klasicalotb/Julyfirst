cb2607d3e78f6e75ada13f90ef52a6284b818154
Pilas01 <peterlass@yahoo.com>
2022-07-21 10:15:14 -0400

    re-update

--- MywebApp/src/main/webapp/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>Hello World, i am a Cloud/Devops Engineer!!</h2>
-</body>
-</html>
--- hello.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ls
-clear
-echo "hello, welcome to pilas cloud devops"
-echo "You are welcome"
-
